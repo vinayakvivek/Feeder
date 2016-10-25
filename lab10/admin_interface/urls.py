8861d56f2b452ecdf52a98cac6f0ad418b1a8809
vinayak <vinayakdeepu@gmail.com>
2016-10-25 21:48:56 +0530

    Added login.html and register.html

--- lab10/admin_interface/urls.py
+++ lab10/admin_interface/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^index/', views.index, name='index'),
+	url(r'^login/', views.user_login, name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- lab10/admin_interface/views.py
+++ lab10/admin_interface/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+
+	# if not request.user.is_authenticated:
+	# 	return render(request, 'login.html', {})
+
 	return render(request, 'index.html', {})
+
+def user_login(request):
+	return render(request, 'login.html', {})
